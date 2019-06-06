9033118fa518560267ca45e70fba20c82da9a283
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2019-06-06 18:16:53 -0400

    questions, quizzes and choices setup

---
+++ api/app/models/questions.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 'bff93b3a-88a3-11e9-bc42-526af7764f64',
+    title: 'When is the birthdate of the Navy?',
+    quizId: '1',
+  },
+  {
+    id: 'bff93fae-88a3-11e9-bc42-526af7764f64',
+    title: 'What are the offical colors of the Navy?',
+    quizId: '1',
+  },
+  {
+    id: 'bff940da-88a3-11e9-bc42-526af7764f64',
+    title: 'When was the battle of Coral Sea?',
+    quizId: '2',
+  },
+  {
+    id: 'bff941fc-88a3-11e9-bc42-526af7764f64',
+    title: 'How many carriers did Japan lose in the Battle at Midway?',
+    quizId: '2',
+  },
+];
